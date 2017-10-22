665510334a8c94d6903334c1c8d6585f0edf58d7
812913 <15928543307@163.com>
2017-10-22 21:45:15 +0800

    add 004-replace-blank-string.js Method 1

---
+++ JavaScript/004-replace-blank-string.js
@@ -0,0 +1,22 @@
+// Use IIFE to prevent global namespace for pollution among Methods
+
+/***************************
+ *   Method 1
+ *   By 812913
+ * */
+
+(function () {
+    function replaceSpace(str)
+    {
+        var str1='';
+        for(var i=0;i<str.length;i++){
+            if(str.charAt(i)==' '){
+                str1+='%20';
+            }else{
+                str1+=str.charAt(i);
+            }
+        }
+        return str1;
+    }
+    replaceSpace("Hello world");
+})();
