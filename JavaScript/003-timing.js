e735f9c28174ef3dfc021d5ade5d8d1576da08a6
swpuLeo <swpu.leo@gmail.com>
2017-10-22 21:02:04 +0800

    modify reverse-string.js

---
+++ JavaScript/003-timing.js
@@ -0,0 +1,27 @@
+// Use IIFE to prevent global namespace for pollution among Methods
+
+/***************************
+ *   Method 1
+ *   By Leo
+ * */
+
+(function () {
+    function timing(){
+        var then = new Date(),
+            hour = 0,
+            min = 0,
+            sec = 0;
+
+        setInterval(time, 1000);
+
+        function time(id) {
+            var now = new Date();
+            var ms = now - then;
+            hour = parseInt(ms/3600000)%60;
+            min = parseInt(ms/60000)%60;
+            sec = parseInt(ms/1000)%60;
+            var oId = document.getElementById(id);
+            oId.innerHTML = '已开始：' + hour + '小时' + min + '分钟' + sec + '秒';
+        }
+    }
+})();
